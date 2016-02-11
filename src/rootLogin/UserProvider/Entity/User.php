155a5d283d17979eaa2d2aa9fd21533ee19ca778
Simon Erhardt <me@rootlogin.ch>
2016-02-11 20:42:26 +0100

    updated entity

--- src/rootLogin/UserProvider/Entity/User.php
+++ src/rootLogin/UserProvider/Entity/User.php
@@ -1,7 +1,30 @@
 <?php
 
+/**
+ * Silex User Provider
+ *
+ *  Copyright 2016 by Simon Erhardt <hello@rootlogin.ch>
+ *
+ * This file is part of the silex user provider.
+ *
+ * The silex user provider is free software: you can redistribute
+ * it and/or modify it under the terms of the Lesser GNU General Public
+ * License version 3 as published by the Free Software Foundation.
+ *
+ * The silex user provider is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the Lesser GNU General Public
+ * License along with the silex user provider.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @license LGPL-3.0 <http://spdx.org/licenses/LGPL-3.0>
+ */
+
 namespace rootLogin\UserProvider\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 
 /**
