f02f396a9a37ed98c1a0f26aaa3c02e9cfab2522
Simon Erhardt <me@rootlogin.ch>
2016-02-22 15:40:53 +0100

    WIP: Move to symfony form

--- src/rootLogin/UserProvider/Form/Type/UserType.php
+++ src/rootLogin/UserProvider/Form/Type/UserType.php
@@ -1,11 +1,36 @@
 <?php
 
+/**
+ * Silex User Provider
+ *
+ *  Copyright 2016 by Simon Erhardt <hello@rootlogin.ch>
+ *
+ * This file is part of the silex user provider.
+ *
+ * The silex user provider is free software: you can redistribute
+ * it and/or modify it under the terms of the Lesser GNU General Public
+ * License version 3 as published by the Free Software Foundation.
+ *
+ * The silex user provider is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the Lesser GNU General Public
+ * License along with the silex user provider.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @license LGPL-3.0 <http://spdx.org/licenses/LGPL-3.0>
+ */
+
 namespace rootLogin\UserProvider\Form\Type;
 
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
@@ -18,13 +43,7 @@ class UserType extends AbstractType
         parent::buildForm($builder, $options);
 
         $builder
-            ->add("salutation","choice", array(
-                'choices'  => array('f' => 'Female', 'm' => 'Male')
-            ))
-            ->add("prename","text", array(
-                "constraints" => array(new Assert\NotBlank())
-            ))
-            ->add("lastname","text", array(
+            ->add("name","text", array(
                 "constraints" => array(new Assert\NotBlank())
             ))
             ->add("email","email", array(
@@ -33,6 +52,15 @@ class UserType extends AbstractType
                     "checkMX" => true
                 ))),
             ))
+            ->add("plainPassword", 'repeated', [
+                'type' => 'password',
+                'invalid_message' => 'The password fields must match.',
+                'options' => array('attr' => array('class' => 'password-field')),
+                'required' => true,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password')
+            ])
+            ->add('register', 'submit');
         ;
     }
 
@@ -41,6 +69,12 @@ class UserType extends AbstractType
         $resolver->setDefaults(array(
             'data_class' => 'rootLogin\UserProvider\Entity\User',
         ));
+    }
+
+    // BC for SF < 2.7
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $this->configureOptions($resolver);
     }
 
     public function getName()
